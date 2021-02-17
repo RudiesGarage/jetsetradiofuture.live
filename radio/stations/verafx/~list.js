4d874a74a52c66d1dc072c97b9cfc5b9d02302ba
Mcdonoughd <dannmcdonough@gmail.com>
2021-02-16 20:58:03 -0500

    added verafx

---
+++ radio/stations/verafx/~list.js
@@ -0,0 +1,43 @@
+//Choose a name for the station
+stationName = "verafx";
+
+//Add it to the array of stations
+stationsArray[stationsArray.length] = stationName;
+
+//Define an array for tracks
+this[stationName+"_tracks"] = new Array();
+
+
+//TRACKS
+this[stationName+'_tracks'][this[stationName+'_tracks'].length] = "Vera FX - Bubbles (Bonus)";
+this[stationName+'_tracks'][this[stationName+'_tracks'].length] = "Vera FX - Crazy World (Bonus)";
+this[stationName+'_tracks'][this[stationName+'_tracks'].length] = "Vera FX - Funk Friday (Bonus)";
+this[stationName+'_tracks'][this[stationName+'_tracks'].length] = "VeraFX - Devil's Contract";
+this[stationName+'_tracks'][this[stationName+'_tracks'].length] = "VeraFX - Dognapped";
+this[stationName+'_tracks'][this[stationName+'_tracks'].length] = "VeraFX - Golden Plan";
+this[stationName+'_tracks'][this[stationName+'_tracks'].length] = "VeraFX - Graffiti Bombing";
+this[stationName+'_tracks'][this[stationName+'_tracks'].length] = "VeraFX - I'll Shock Ya";
+this[stationName+'_tracks'][this[stationName+'_tracks'].length] = "VeraFX - Jet Set Freedom";
+this[stationName+'_tracks'][this[stationName+'_tracks'].length] = "VeraFX - Jet Set Revolution Intro";
+this[stationName+'_tracks'][this[stationName+'_tracks'].length] = "VeraFX - Just Your Problem";
+this[stationName+'_tracks'][this[stationName+'_tracks'].length] = "VeraFX - Keisatsu";
+this[stationName+'_tracks'][this[stationName+'_tracks'].length] = "VeraFX - Noise Nostalgia";
+this[stationName+'_tracks'][this[stationName+'_tracks'].length] = "VeraFX - Streets of Reggae";
+this[stationName+'_tracks'][this[stationName+'_tracks'].length] = "VeraFX - The Animal";
+this[stationName+'_tracks'][this[stationName+'_tracks'].length] = "VeraFX - Vengeance";
+this[stationName+'_tracks'][this[stationName+'_tracks'].length] = "VeraFX - We Own The Streets";
+this[stationName+'_tracks'][this[stationName+'_tracks'].length] = "VeraFX - Whats Blood For";
+this[stationName+'_tracks'][this[stationName+'_tracks'].length] = "VeraFX - Your Love";
+
+
+
+
+
+
+
+
+
+
+
+
+
