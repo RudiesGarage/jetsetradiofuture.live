97d6d74d35839b112780b5c72d7cfccef677a2aa
Mcdonoughd <dannmcdonough@gmail.com>
2022-03-06 22:41:57 -0500

    updated crazytaxi station

--- radio/stations/crazytaxi/~list.js
+++ radio/stations/crazytaxi/~list.js
@@ -1,4 +1,3 @@
-
 //Choose a name for the station
 stationName = "crazytaxi";
 
@@ -10,7 +9,6 @@ this[stationName+"_tracks"] = new Array();
 
 
 //TRACKS
-
 this[stationName+'_tracks'][this[stationName+'_tracks'].length] = "Bad Religion - Empty Causes";
 this[stationName+'_tracks'][this[stationName+'_tracks'].length] = "Bad Religion - Hear It";
 this[stationName+'_tracks'][this[stationName+'_tracks'].length] = "Bad Religion - Inner Logic";
@@ -19,11 +17,22 @@ this[stationName+'_tracks'][this[stationName+'_tracks'].length] = "Bad Religion 
 this[stationName+'_tracks'][this[stationName+'_tracks'].length] = "Bad Religion - Them And Us";
 this[stationName+'_tracks'][this[stationName+'_tracks'].length] = "Brian Setzer - Hot Rod Girl";
 this[stationName+'_tracks'][this[stationName+'_tracks'].length] = "Brian Setzer - Ignition";
+this[stationName+'_tracks'][this[stationName+'_tracks'].length] = "Burn the Fields - Escape Artist";
+this[stationName+'_tracks'][this[stationName+'_tracks'].length] = "Jonny Rumble - Black Radio";
+this[stationName+'_tracks'][this[stationName+'_tracks'].length] = "Lions Lions - Radiator";
 this[stationName+'_tracks'][this[stationName+'_tracks'].length] = "Methods Of Mayhem - Crash";
 this[stationName+'_tracks'][this[stationName+'_tracks'].length] = "Methods Of Mayhem - Who The Hell Cares";
+this[stationName+'_tracks'][this[stationName+'_tracks'].length] = "One Light Out - Orange Wednesday";
+this[stationName+'_tracks'][this[stationName+'_tracks'].length] = "Pivit - Fingercuffs";
+this[stationName+'_tracks'][this[stationName+'_tracks'].length] = "Pivit - Middle Children";
 this[stationName+'_tracks'][this[stationName+'_tracks'].length] = "SilverBullit - Boom Boom Boogie";
 this[stationName+'_tracks'][this[stationName+'_tracks'].length] = "SilverBullit - King of the Line";
 this[stationName+'_tracks'][this[stationName+'_tracks'].length] = "SilverBullit - Raw Power Angel";
+this[stationName+'_tracks'][this[stationName+'_tracks'].length] = "The Days The Nights - Radical Sabbatical";
+this[stationName+'_tracks'][this[stationName+'_tracks'].length] = "The Hooks - Get Out";
+this[stationName+'_tracks'][this[stationName+'_tracks'].length] = "The Juliet Dagger - Flinch";
+this[stationName+'_tracks'][this[stationName+'_tracks'].length] = "The Juliet Dagger - Ragdoll";
+this[stationName+'_tracks'][this[stationName+'_tracks'].length] = "The Juliet Dagger - Taking It Back";
 this[stationName+'_tracks'][this[stationName+'_tracks'].length] = "The Offspring - All I Want";
 this[stationName+'_tracks'][this[stationName+'_tracks'].length] = "The Offspring - Americana";
 this[stationName+'_tracks'][this[stationName+'_tracks'].length] = "The Offspring - Change The World";
@@ -34,3 +43,22 @@ this[stationName+'_tracks'][this[stationName+'_tracks'].length] = "The Offspring
 this[stationName+'_tracks'][this[stationName+'_tracks'].length] = "The Offspring - Walla Walla";
 this[stationName+'_tracks'][this[stationName+'_tracks'].length] = "The Offspring - Want You Bad";
 this[stationName+'_tracks'][this[stationName+'_tracks'].length] = "The Offspring - Way Down The Line";
+this[stationName+'_tracks'][this[stationName+'_tracks'].length] = "The Vicious Five - Your Mouth Is A Guillotine";
+this[stationName+'_tracks'][this[stationName+'_tracks'].length] = "Theron Day - The Chase";
+this[stationName+'_tracks'][this[stationName+'_tracks'].length] = "Three Against Four - Jump Bink";
+this[stationName+'_tracks'][this[stationName+'_tracks'].length] = "Too Rude - The Distance";
+this[stationName+'_tracks'][this[stationName+'_tracks'].length] = "Total Chaos - Let It Roll";
+this[stationName+'_tracks'][this[stationName+'_tracks'].length] = "Total Chaos - What You Gonna Do";
+this[stationName+'_tracks'][this[stationName+'_tracks'].length] = "Various - Name Loop";
+this[stationName+'_tracks'][this[stationName+'_tracks'].length] = "We Are Invisible - Spy Chaser";
+
+
+
+
+
+
+
+
+
+
+
