d4b93309080f292d457e91410ad60052633597b8
Mcdonoughd <dannmcdonough@gmail.com>
2022-04-02 22:11:06 -0400

    add assets

--- refactor/public/scripts/index.js
+++ refactor/public/scripts/index.js
@@ -21,7 +21,7 @@ const pauseBtn = document.getElementById("pause-btn");
 const progressBar = document.getElementById('playbar-progress');
 const largePlayIcon = document.getElementById("large-play-icon");
 
-// const rightGainBar = document.getElementById("right-gain-bar");
+const gainBarValue = document.getElementById("gain-bar");
 const personalLinks = document.getElementById("personal-links");
 const personalLinksContainer = document.getElementById("personal-links-container");
 const projectName = document.getElementById("project-name");
@@ -174,142 +174,10 @@ const createContext = () => {
 document.getElementById('radio-link').onclick = () => {
     radioModal.style.display = "block";
 };
-document.getElementById('radio-close-modal').onclick = () => {
-    radioModal.style.display = "";
-};
 
-// Variables to target our base class,  get carousel items, count how many carousel items there are, set the slide to 0 (which is the number that tells us the frame we're on), and set motion to true which disables interactivity.
-var itemClassName = "station-grid-wrapper carousel__photo";
-items = document.getElementsByClassName(itemClassName),
-    totalItems = items.length,
-    slide = 0,
-    moving = true;
 
-// To initialise the carousel we'll want to update the DOM with our own classes
-function setInitialClasses() {
 
-    // Target the last, initial, and next items and give them the relevant class.
-    // This assumes there are three or more items.
-    items[totalItems - 1].classList.add("prev");
-    items[0].classList.add("active");
-    items[1].classList.add("next");
-}
 
-// Set click events to navigation buttons
-
-function setEventListeners() {
-    var next = document.getElementsByClassName('carousel__button--next')[0],
-        prev = document.getElementsByClassName('carousel__button--prev')[0];
-
-    next.addEventListener('click', moveNext);
-    prev.addEventListener('click', movePrev);
-}
-
-// Disable interaction by setting 'moving' to true for the same duration as our transition (0.5s = 500ms)
-function disableInteraction() {
-    moving = true;
-
-    setTimeout(function() {
-        moving = false
-    }, 500);
-}
-
-function moveCarouselTo(slide) {
-
-    // Check if carousel is moving, if not, allow interaction
-    if (!moving) {
-
-        // temporarily disable interactivity
-        disableInteraction();
-
-        // Preemptively set variables for the current next and previous slide, as well as the potential next or previous slide.
-        var newPrevious = slide - 1,
-            newNext = slide + 1,
-            oldPrevious = slide - 2,
-            oldNext = slide + 2;
-
-        // Test if carousel has more than three items
-        if ((totalItems - 1) > 3) {
-
-            // Checks if the new potential slide is out of bounds and sets slide numbers
-            if (newPrevious <= 0) {
-                oldPrevious = (totalItems - 1);
-            } else if (newNext >= (totalItems - 1)) {
-                oldNext = 0;
-            }
-
-            // Check if current slide is at the beginning or end and sets slide numbers
-            if (slide === 0) {
-                newPrevious = (totalItems - 1);
-                oldPrevious = (totalItems - 2);
-                oldNext = (slide + 1);
-            } else if (slide === (totalItems - 1)) {
-                newPrevious = (slide - 1);
-                newNext = 0;
-                oldNext = 1;
-            }
-
-            // Now we've worked out where we are and where we're going, by adding and removing classes, we'll be triggering the carousel's transitions.
-
-            // Based on the current slide, reset to default classes.
-            items[oldPrevious].className = itemClassName;
-            items[oldNext].className = itemClassName;
-
-            // Add the new classes
-            items[newPrevious].className = itemClassName + " prev";
-            items[slide].className = itemClassName + " active";
-            items[newNext].className = itemClassName + " next";
-        }
-    }
-}
-
-// Next navigation handler
-function moveNext() {
-
-    // Check if moving
-    if (!moving) {
-
-        // If it's the last slide, reset to 0, else +1
-        if (slide === (totalItems - 1)) {
-            slide = 0;
-        } else {
-            slide++;
-        }
-
-        // Move carousel to updated slide
-        moveCarouselTo(slide);
-    }
-}
-
-// Previous navigation handler
-function movePrev() {
-
-    // Check if moving
-    if (!moving) {
-
-        // If it's the first slide, set as the last slide, else -1
-        if (slide === 0) {
-            slide = (totalItems - 1);
-        } else {
-            slide--;
-        }
-
-        // Move carousel to updated slide
-        moveCarouselTo(slide);
-    }
-}
-
-// Initialise carousel
-function initCarousel() {
-    setInitialClasses();
-    setEventListeners();
-
-    // Set moving to false now that the carousel is ready
-    moving = false;
-}
-
-// make it rain
-initCarousel();
 
 // Keyboard Controls Modal Control
 document.getElementById('keyboard-controls').onclick = () => {
@@ -409,31 +277,8 @@ document.getElementById('large-play').onclick = () => {
     switchPlayPause();
 };
 
-/*  const switchFullScreen = () => {
-        if (!fscreen.fullscreenElement) {
-            fscreen.requestFullscreen(document.documentElement);
-        } else {
-            fscreen.exitFullscreen();
-        }
-    };
 
-    enterExitFullScreen.onclick = () => {
-        switchFullScreen();
-    };
-
-    fscreen.onfullscreenchange = (() => {
-        if (!!fscreen.fullscreenElement) {
-            enterExitFullScreen.innerHTML = `<i class="fas fa-compress-arrows-alt"></i>`;
-            enterExitFullScreen.setAttribute("data", "Exit Full-Screen, or press [F]");
-        } else {
-            enterExitFullScreen.innerHTML = `<i class="fas fa-arrows-alt"></i>`;
-            enterExitFullScreen.setAttribute("data", "Enter Full-Screen, or press [F]");
-        }
-    });
-*/
-
-
-document.onkeyup = (e) => {
+document.onkeydown = (e) => {
     e.preventDefault();
     if (audio.src !== "") {
 
@@ -497,8 +342,14 @@ document.onkeyup = (e) => {
 
 //TODO update volume
 const updateGain = (value) => {
+    if (!contextCreated) {
+        createContext();
+    }
+    while (value > 1) {
+        value = value / 10;
+    }
     gain.gain.value = value;
-    //gainBarValue.style.width = `${193 * value}px`;
+    gainBarValue.value = value * 10;
 };
 
 // document.getElementById("gain-bar").onclick = (e) => {
